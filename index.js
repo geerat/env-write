16cde79882b1d36dfdf5bfe748bdb1ae84196a5b
Chester Jerrat <chester.jerrat@beca.com>
2021-02-05 16:09:04 +1300

    added generateAndDownloadEnv

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 module.exports = new EnvClass();
 
+
+
+
 class EnvClass {
     generateENV(data) {
-        return new Blob(data, {type: 'text/plain;charset=utf-8'});
+        const keys = Object.keys(data);
+        let stringStream = '';
+        for(let i=0; i < keys.length; i++) {
+            stringStream = stringStream + keys[i] + ' = \'' + data[keys[i]] + '\'\r\n';
+        }
+        return new Blob([stringStream], {type: 'text/plain;charset=utf-8'});
+    }
+
+    generateAndDownloadENV(data, fileName) {
+        const element = document.createElement("a");
+        const file = this.generateENV(data);
+        element.href = URL.createObjectURL(file);
+        element.download = fileName + ".env";
+        document.body.appendChild(element);
+        element.click();
     }
 }
